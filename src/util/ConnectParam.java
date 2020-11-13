d66376864a5673f18d89ab03a57bb4982d60378a
ebundhoo <44093400+ebundhoo@users.noreply.github.com>
2020-11-13 15:40:12 +0400

    class ConnectParam

---
+++ src/util/ConnectParam.java
@@ -0,0 +1,37 @@
+package util;
+
+/**
+ * This class represents the connection parameters to the PostgreSQL JDBC driver.
+ * 
+ * @author 	Eshan Bundhoo, University of Mauritius
+ * @since	2019-02-19
+ * @version	1.0
+ *
+ */
+
+public class ConnectParam {
+	
+	private String url;
+	private String username;
+	private String password;
+	
+	public ConnectParam(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+
+}
